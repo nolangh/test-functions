f141e9a8c3009f5ad625506b904284a46f35a1fc
nolangh <nolanghiggins@gmail.com>
2021-03-08 13:55:22 -0700

    added a basic function to aprox age input

--- age.js
+++ age.js
@@ -1,4 +1,8 @@
-function sayHello() {
-    alert("Hello, welcome to this website!");
+//function that takes in user age and prints out a string and users aprox age
+function age(birth) {
+    // birth is the "argument" passed into the function, user input for this case
+    let yearBorn = 2021 - `${birth}`
+        // yearBorn is the variable that will take in "birth" and subtract it from 2021"
+    return 'You are ' + yearBorn + ' years old'
+        // we return a string and yearBorn to the user
 }
-random Text
