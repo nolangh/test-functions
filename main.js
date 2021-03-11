83cd399a335180b91b006a4cc79b4e413340a9f8
nolangh <nolanghiggins@gmail.com>
2021-03-10 18:39:25 -0700

    added function for nav animation

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+/* ------------------- document.querySelector('.menu-btn') selects the class menu-btn  ------------------ */
 const menuBtn = document.querySelector('.menu-btn');
-
+/* ------------ This sets the class menuOpen and sets it to false ----------- */
 let menuOpen = false;
-
+/* ------------------- This adds event listener to menuBtn ------------------ */
 menuBtn.addEventListener('click', () => {
-        if (!menuOpen) {
-            menuBtn.classList.add('open');
-            menuOpen = true;
-        } else {
-            menuBtn.classList.remove('open');
-            menuOpen = false;
-        }
+    if (!menuOpen) {
+        /* --------------- This added a class to menuBtn if it is open -------------- */
+        menuBtn.classList.add('open');
+        menuOpen = true;
+        /* ------------- this removes class open from menuBtn if closed ------------- */
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
     }
-};
+});
