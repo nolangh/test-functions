c4a94f9ecfd3625e25e7f0cab5af7f399275c370
nolangh <nolanghiggins@gmail.com>
2021-03-11 16:33:19 -0700

    did things i guess

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 /* ------------------- document.querySelector('.menu-btn') selects the class menu-btn  ------------------ */
 const menuBtn = document.querySelector('.menu-btn');
-/* ------------ This sets the class menuOpen and sets it to false ----------- */
+const navCard = document.querySelector('.nav-card')
+    /* ------------ This sets the class menuOpen and sets it to false ----------- */
 let menuOpen = false;
 /* ------------------- This adds event listener to menuBtn ------------------ */
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         /* --------------- This added a class to menuBtn if it is open -------------- */
         menuBtn.classList.add('open');
+        navCard.classList.add('open');
         menuOpen = true;
         /* ------------- this removes class open from menuBtn if closed ------------- */
     } else {
         menuBtn.classList.remove('open');
         menuOpen = false;
+        navCard.classList.remove('open');
     }
 });
